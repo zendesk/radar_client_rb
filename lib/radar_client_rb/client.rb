fe1ec08860c9caf1b198d1df9058186039ed3aab
jden <jden@zendesk.com>
2016-03-01 16:10:28 -0800

    Add Radar::Client.logger

--- lib/radar_client_rb/client.rb
+++ lib/radar_client_rb/client.rb
@@ -2,3 +2,24 @@ require_relative './legacy_client.rb'
 
 # Backwards compatibility with 1.0.4
 Radar::Client = Radar::LegacyClient
+
+module Radar
+  class Client
+    def self.logger
+      @logger ||= if const_defined?("Rails")
+        Rails.logger
+      elsif const_defined?("ActiveRecord")
+        ActiveRecord::Base.logger
+      else
+        Logger.new(STDOUT)
+      end
+    end
+
+    def self.log(message)
+      logger.info("Radar::Client #{message}")
+    end
+    def self.log_debug(message)
+      logger.debug("Radar::Client #{message}")
+    end
+  end
+end
--- test/minitest_helper.rb
+++ test/minitest_helper.rb
@@ -1,2 +1,4 @@
 require 'minitest/autorun'
 require 'mocha/setup'
+
+Radar::Client.logger.level = 5 unless ENV['VERBOSE']
